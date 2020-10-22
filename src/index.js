6bb9a1cc1f13bd2315045d74f3f94e21da94da42
Dima <dimasilolil@gmail.com>
2020-10-22 22:31:24 +0600

    Dropdown items

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 window.$ = require('jquery')
+
 $(document).ready(function () {
-	alert("ы")
+    $(".select-arrow").on('click', function (el) {
+        let id = $(el.target).attr('id').replace(/\D/g, '');
+        let menu = $("#menu" + id);
+        let border = $("#border" + id);
+        if (menu.is(":visible")) {
+            menu.hide();
+            border.css('border-radius','4px')
+        } else {
+            menu.show();
+            border.css('border-radius','0')
+            border.css('border-top-left-radius','4px')
+            border.css('border-top-right-radius','4px')
+        }
+    })
 })
